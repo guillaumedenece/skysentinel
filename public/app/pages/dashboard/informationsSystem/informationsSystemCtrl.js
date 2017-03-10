21dac3bbd6e80ad0d8edcf6d508261df3f4781a0
guillaumedenece <guillaume.denece@edu.ece.fr>
2017-03-10 16:00:42 +0100

    resolution probleme temps reel dashboard

--- public/app/pages/dashboard/informationsSystem/informationsSystemCtrl.js
+++ public/app/pages/dashboard/informationsSystem/informationsSystemCtrl.js
@@ -40,25 +40,25 @@
       })
 
       dashboardInformationsSystemFactorySocket.on('logGroundStation',function(log){
-        $scope.doorState = informations[0].doorState;
-        $scope.elevatorState = informations[0].elevatorState;
+        $scope.doorState = log.doorState;
+        $scope.elevatorState = log.elevatorState;
         $scope.weatherInfos =new Object();
-        $scope.weatherInfos.rain = informations[0].weatherInfos.rain
-        $scope.weatherInfos.wind = informations[0].weatherInfos.wind
-        $scope.weatherInfos.humidity = informations[0].weatherInfos.humidity
-        $scope.weatherInfos.temperature = informations[0].weatherInfos.temperature
+        $scope.weatherInfos.rain = log.weatherInfos.rain
+        $scope.weatherInfos.wind = log.weatherInfos.wind
+        $scope.weatherInfos.humidity = log.weatherInfos.humidity
+        $scope.weatherInfos.temperature = log.weatherInfos.temperature
         $scope.batteriesInfos = new Array()
         for(var i=0; i<4; i++)
         {
           $scope.batteriesInfos[i] = new Object()
-          $scope.batteriesInfos[i].batterySlot = informations[0].batteriesInfos[i].batterySlot
-          $scope.batteriesInfos[i].batteryVoltage = informations[0].batteriesInfos[i].batteryVoltage
-          $scope.batteriesInfos[i].batteryPresence = informations[0].batteriesInfos[i].batteryPresence
+          $scope.batteriesInfos[i].batterySlot = log.batteriesInfos[i].batterySlot
+          $scope.batteriesInfos[i].batteryVoltage = log.batteriesInfos[i].batteryVoltage
+          $scope.batteriesInfos[i].batteryPresence = log.batteriesInfos[i].batteryPresence
           console.log(JSON.stringify($scope.batteriesInfos[i]))
         }
-        // $scope.boxInfos.humidity = informations[0].boxInfos.humidity
-        // $scope.boxInfos.temperature = informations[0].boxInfos.temperature
-        // $scope.boxInfos.pressure = informations[0].boxInfos.pressure
+        $scope.boxInfos.humidity = log.boxInfos.humidity
+        $scope.boxInfos.temperature = log.boxInfos.temperature
+        $scope.boxInfos.pressure = log.boxInfos.pressure
 
         imageManager();
 
